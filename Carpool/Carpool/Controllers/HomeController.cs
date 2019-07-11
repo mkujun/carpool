81bdd7a60de3a38a477ecbf00a68cf3455f4992e
mkujun <kujundzic.marko@hotmail.com>
2019-07-11 12:43:45 +0200

    removed unused files

--- Carpool/Carpool/Controllers/HomeController.cs
+++ Carpool/Carpool/Controllers/HomeController.cs
@@ -39,10 +39,5 @@ namespace Carpool.Controllers
             return View(cars);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
