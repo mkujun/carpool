eab0b864c699ba15d89131796e7b7b26752f25c2
mkujun <kujundzic.marko@hotmail.com>
2019-07-11 10:10:12 +0200

    added basic domain classes

---
+++ Carpool/Carpool/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Carpool.Models
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public string CarType { get; set; }
+        public string Color { get; set; }
+        public string Plates { get; set; }
+        public int NumberOfSeats { get; set; }
+    }
+}
---
+++ Carpool/Carpool/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Carpool.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDriver { get; set; }
+    }
+}
