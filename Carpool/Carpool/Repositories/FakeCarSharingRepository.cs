1b88b6cda5c1072d54af027a8418fce6bf697106
mkujun <kujundzic.marko@hotmail.com>
2019-07-16 13:21:31 +0200

    added few inputs to car sharing fake repository

--- Carpool/Carpool/Repositories/FakeCarSharingRepository.cs
+++ Carpool/Carpool/Repositories/FakeCarSharingRepository.cs
@@ -11,6 +11,38 @@ namespace Carpool.Repositories
     {
         public List<CarSharing> CarSharings { get; set; }
 
+        public FakeCarSharingRepository()
+        {
+            CarSharings.Add(new CarSharing("Rijeka", "Zagreb", DateTime.Now, DateTime.Today));
+            CarSharings.Add(new CarSharing("Crikvenica", "Zagreb", DateTime.Now, DateTime.Today));
+        }
+
+        public bool IsCarAlreadyOnTheRide(string licensePlates, DateTime startDate, DateTime endDate)
+        {
+            if (CarSharings != null)
+            {
+                List<CarSharing> carOnTheRoad = CarSharings.Where(c => c.SelectedCarPlates == licensePlates).ToList(); 
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            /*
+            if(carAlreadyOnTheRoad)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+            */
+        }
+
         public void SaveCarSharing(CarSharing carSharing)
         {
             // todo : finish this adding, find if that car sharing ride is possible...
